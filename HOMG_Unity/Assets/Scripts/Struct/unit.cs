99786df9a4783f0befd4a7b63ac09d777519347f
MilkMangoTea <blockbyblock@qq.com>
2025-10-06 02:49:35 +0800

    重构controller与render，独立unit，避免了点击到ui之后

---
+++ HOMG_Unity/Assets/Scripts/Struct/unit.cs
@@ -0,0 +1,27 @@
+﻿[System.Serializable]
+public class Unit
+{
+    private UnitData _data;
+    private string _name;
+
+    public UnitData GetData()
+    {
+        return _data;
+    }
+
+    public string GetName()
+    {
+        return _name;
+    }
+
+    public UnitData UnitData => GetData();
+    public string Type => GetData().Type;
+    public string Name => GetName();
+    public int ATK => GetData().ATK;
+    public int DEF => GetData().DEF;
+
+    public Unit(string unitType)
+    {
+        _data = GameApp.ControllerManager.GetController(ControllerType.Game).GetModel<MapModel>().mapData.unitManager.GetUnitData(unitType);
+    }
+}
